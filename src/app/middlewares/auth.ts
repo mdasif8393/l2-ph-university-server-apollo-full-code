01c12f01cb97cef33f73bb8790c2219c9a10fd1b
mdasif8393 <md.asif.8393@gmail.com>
2025-01-13 23:54:36 +0600

    add try catch

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -15,12 +15,16 @@ const auth = (...requiredRoles: TUserRole[]) => {
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
-
-    // checking if the given token is valid
-    const decoded = jwt.verify(
-      token,
-      config.jwt_access_secret as string,
-    ) as JwtPayload;
+    let decoded;
+    try {
+      // checking if the given token is valid
+      decoded = jwt.verify(
+        token,
+        config.jwt_access_secret as string,
+      ) as JwtPayload;
+    } catch (err) {
+      throw new AppError(httpStatus.UNAUTHORIZED, 'Unauthorized');
+    }
 
     const { role, userId, iat } = decoded;
 
